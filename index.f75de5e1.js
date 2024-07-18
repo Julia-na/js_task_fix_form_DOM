6a4d2a15ea1ca4a4d202d51c5efc3fc5052e0b1b
Julia Churkina <curkinau980@gmail.com>
2024-07-18 22:22:15 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,22 @@
+"use strict";
+const input = [
+    ...document.querySelectorAll("input")
+];
+function wordSpace(word) {
+    let wordWithSpace = word[0].toUpperCase();
+    for(let i = 1; i < word.length; i++){
+        if (word[i] === word[i].toUpperCase()) wordWithSpace += " ";
+        wordWithSpace += word[i];
+    }
+    return wordWithSpace;
+}
+for (const el of input){
+    const label = document.createElement("label");
+    el.setAttribute("placeholder", wordSpace(el.name));
+    label.textContent = wordSpace(el.name);
+    label.className = "field-label";
+    label.setAttribute("for", el.id);
+    el.before(label);
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
